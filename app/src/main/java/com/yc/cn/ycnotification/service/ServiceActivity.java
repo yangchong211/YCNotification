e093bfa11bc633e720709386e9ef16fa2ee37bb5
yangchong <yangchong211@163.com>
2021-09-10 11:35:19 +0800

    完善案例

--- app/src/main/java/com/yc/cn/ycnotification/service/ServiceActivity.java
+++ app/src/main/java/com/yc/cn/ycnotification/service/ServiceActivity.java
@@ -1,15 +1,23 @@
 package com.yc.cn.ycnotification.service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import com.yc.cn.ycnotification.MainActivity;
 import com.yc.cn.ycnotification.R;
+import com.yc.cn.ycnotification.TestActivity;
 
 public class ServiceActivity extends AppCompatActivity {
 
@@ -17,6 +25,8 @@ public class ServiceActivity extends AppCompatActivity {
     private TextView tv2;
     private TextView tv3;
     private TextView tv4;
+    private TextView tv5;
+    private TextView tv6;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +38,8 @@ public class ServiceActivity extends AppCompatActivity {
         tv2 = findViewById(R.id.tv_2);
         tv3 = findViewById(R.id.tv_3);
         tv4 = findViewById(R.id.tv_4);
+        tv5 = findViewById(R.id.tv_5);
+        tv6 = findViewById(R.id.tv_6);
 
         tv1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,5 +67,58 @@ public class ServiceActivity extends AppCompatActivity {
                 MyService2.stopRiderService(ServiceActivity.this);
             }
         });
+
+        tv5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send();
+            }
+        });
+        tv6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               if (mNotificationManager!=null){
+                   mNotificationManager.cancel(DIALOG_TIME_SEC);
+               }
+            }
+        });
     }
+
+    private NotificationManager mNotificationManager;
+    private static final String LOCAL_CHANNEL_ID = "RIDER_LOCAL_CHANNEL_ID";
+    public static final int DIALOG_TIME_SEC = 2;
+
+    private void send(){
+        // 创建一个NotificationManager的引用
+        mNotificationManager = (NotificationManager)
+                this.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel mChannel = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            mChannel = new NotificationChannel(LOCAL_CHANNEL_ID,
+                    "Channel Notifications", NotificationManager.IMPORTANCE_HIGH);
+            //  mChannel.setDescription(description);
+            //  mChannel.enableLights(true);
+            //  mChannel.setLightColor(Color.RED);
+            //  mChannel.enableVibration(true);
+            //  mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
+        Intent intent = new Intent(this, TestActivity.class);
+        //添加为栈顶Activity
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new NotificationCompat.Builder(this, LOCAL_CHANNEL_ID)
+                .setContentTitle("Title")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentTitle("您有一条新通知")
+                .setContentText("这是一条逗你玩的消息")
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setFullScreenIntent(pendingIntent, true)
+                .build();
+        mNotificationManager.notify(DIALOG_TIME_SEC, notification);
+    }
+
 }
